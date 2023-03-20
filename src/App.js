7972f30606622352077b9e60c659404ce93414fd
Satyendra Mourya <66292112+niraj202@users.noreply.github.com>
2023-03-20 19:26:16 +0530

    revert

--- src/App.js
+++ src/App.js
@@ -2,21 +2,14 @@ import React, { Suspense } from "react";
 import "./App.css";
 
 import {
-  // Blog,
+  Blog,
   Footer,
-  // Possibility,
-  // Features,
-  // WhatGPT3,
+  Possibility,
+  Features,
+  WhatGPT3,
   Header,
 } from "./containers";
 import { Cta, Brand, Navbar } from "./components";
-
-const Blog = React.lazy(() => import("./containers/blog/Blog"));
-const Possibility = React.lazy(() =>
-  import("./containers/possibility/Possibility")
-);
-const Features = React.lazy(() => import("./containers/features/Features"));
-const WhatGPT3 = React.lazy(() => import("./containers/whatGPT3/WhatGPT3"));
 
 const App = () => {
   return (
@@ -26,19 +19,11 @@ const App = () => {
         <Header />
       </div>
       <Brand />
-      <Suspense fallback={<div>Loading...</div>}>
-        <WhatGPT3 />
-      </Suspense>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Features />
-      </Suspense>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Possibility />
-      </Suspense>
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
       <Cta />
-      <Suspense fallback={<div>Loading...</div>}>
-        <Blog />
-      </Suspense>
+      <Blog />
       <Footer />
     </div>
   );
