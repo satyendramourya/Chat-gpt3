6ece8627cafdb48ded2d555d7b6ca205885c8ba0
Satyendra Mourya <66292112+niraj202@users.noreply.github.com>
2023-03-20 19:06:26 +0530

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,29 +1,47 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./App.css";
 
 import {
-  Blog,
+  // Blog,
   Footer,
-  Possibility,
-  Features,
-  WhatGPT3,
-  Header,
+  // Possibility,
+  // Features,
+  // WhatGPT3,
+  // Header,
 } from "./containers";
-import { Cta, Brand, Article, Feature, Navbar } from "./components";
+import { Cta, Brand, Navbar } from "./components";
+
+const Blog = React.lazy(() => import("./containers/blog/Blog"));
+const Possibility = React.lazy(() =>
+  import("./containers/possibility/Possibility")
+);
+const Features = React.lazy(() => import("./containers/features/Features"));
+const WhatGPT3 = React.lazy(() => import("./containers/whatGPT3/WhatGPT3"));
+const Header = React.lazy(() => import("./containers/header/Header"));
 
 const App = () => {
   return (
     <div className="App">
       <div className="gradient__bg">
         <Navbar />
-        <Header />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Header />
+        </Suspense>
       </div>
       <Brand />
-      <WhatGPT3 />
-      <Features />
-      <Possibility />
+      <Suspense fallback={<div>Loading...</div>}>
+        <WhatGPT3 />
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Features />
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Possibility />
+      </Suspense>
       <Cta />
-      <Blog />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Blog />
+      </Suspense>
       <Footer />
     </div>
   );
